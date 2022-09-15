d79e123540a61bb597be454d0fe78f542cc544bd
hithaisri <hithaisri.97@gmail.com>
2022-09-15 19:32:42 +0530

    book-reader service changes

---
+++ Books-Reader/src/main/java/com/reader/demo/BooksReaderApplication.java
@@ -0,0 +1,13 @@
+package com.reader.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksReaderApplication.class, args);
+	}
+
+}
---
+++ Books-Reader/src/test/java/com/reader/demo/BooksReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reader.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
